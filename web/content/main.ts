082ffd0f449365c5c146cf74c620ab0aa8ec09c0
Alex Iadicicco <alex@ajitek.net>
2021-03-22 23:41:44 -0700

    "We have react/redux at home" The react/redux at home:

--- web/content/main.ts
+++ web/content/main.ts
@@ -12,113 +12,110 @@ import {
 
 const { GET, POST, DELETE } = createClient(window.fetch);
 
-const T = (s: string): Node => document.createTextNode(s);
-const E = (s: string) => (...children: Node[]): Element =>
-    K(document.createElement(s), (e) =>
-        children.forEach((c) => e.appendChild(c))
-    );
-const A = <T extends Element>(x: T, attrs: object): T =>
-    K(x, (e) =>
-        Object.entries(attrs).forEach(([k, v]) => e.setAttribute(k, v))
-    );
-const K = <T>(x: T, f: (x: T) => void): T => (f(x), x);
+type Action =
+    | { ty: 'startPlaying'; manifold: { ty: string; name: string } }
+    | { ty: 'backToStart' };
+type State =
+    | { at: 'start' }
+    | { at: 'playing'; manifold: { ty: string; name: string } };
+type Dispatch = (action: Action) => void;
 
-const $_ = E('div');
-const $p = E('p');
-const $table = E('table');
-const $b = E('b');
-const $link = (href: string, ...xs: Node[]) => A(E('a')(...xs), { href });
-
-const $a = (onClick: (e: Event) => void, ...xs: Node[]) =>
-    K(E('a')(...xs), (e) => e.addEventListener('click', onClick));
-const $button = (onClick: (e: Event) => void, ...xs: Node[]) =>
-    K(E('button')(...xs), (e) => e.addEventListener('click', onClick));
-
-const $tabularize = (...xs: Node[]): Element =>
-    A($table(...xs), { class: 'tabularize' });
-const $tr = (...xs: Node[]): Element => E('tr')(...xs.map((e) => E('td')(e)));
-
-function render() {
-    function newGame(manifold: string, size: string) {
-        return (e: Event) => {
-            confirm(`Start ${size} ${manifold} game?`);
-        };
-    }
-
-    function row(manifold: string, name: string) {
-        elementOpen('tr');
-
-        elementOpen('td');
-        elementOpen('b');
-        text(name);
-        elementClose('b');
-        elementClose('td');
-
-        for (const size of ['9x9', '13x13', '19x19']) {
-            elementOpen('td');
-            elementOpen(
-                'button',
-                null,
-                null,
-                'onClick',
-                newGame(manifold, size)
-            );
-            text(size);
-            elementClose('button');
-            elementClose('td');
-        }
-
-        elementClose('tr');
-    }
-
-    elementOpen('div');
-    text('Start a game at this table:');
-    elementOpen('table', null, null, 'class', 'tabularize');
-    for (const [manifold, name] of [
-        ['normal', 'Normal'],
-        ['cylinder', 'Cylinder'],
-        ['mobius', 'Mobius strip'],
-        ['torus', 'Torus'],
-        ['klein', 'Klein bottle'],
-        ['projective', 'Projective plane'],
-    ]) {
-        row(manifold, name);
-    }
-    elementClose('table');
-    elementClose('div');
+function initialState(): State {
+    return { at: 'start' };
 }
 
-const newGameMenu = (): Node => {
-    function newGame(manifold: string, size: string) {
-        return (e: Event) => {
-            confirm(`Start ${size} ${manifold} game?`);
-        };
+function reduce(state: State, action: Action): State {
+    switch (action.ty) {
+        case 'startPlaying':
+            return { at: 'playing', manifold: action.manifold };
+        case 'backToStart':
+            return { at: 'start' };
+        default:
+            console.log('unknown action type');
+            return state;
     }
+}
 
-    const $row = (manifold: string, name: string) =>
-        $tr(
-            $b(T(name)),
-            $button(newGame(manifold, '9x9'), T('9x9')),
-            $button(newGame(manifold, '13x13'), T('13x13')),
-            $button(newGame(manifold, '19x19'), T('19x19'))
+type RF = () => void;
+
+const T = (s: string): RF => () => text(s);
+const E = (s: string, ...args: any[]) => (...children: RF[]): RF => () => (
+    elementOpen(s, ...args), children.forEach((f) => f()), elementClose(s)
+);
+
+const $_ = E('div');
+const $b = E('b');
+const $tr = E('tr');
+const $td = E('td');
+
+const tabularize = E('table', null, null, 'class', 'tabularize');
+const button = (hdlr: (e: Event) => void) =>
+    E('button', null, null, 'onclick', hdlr, 'class', 'cls');
+
+const row = (...children: RF[]) => $tr(...children.map((c) => $td(c)));
+
+const renderAt = {
+    start: (state: State, dispatch: Dispatch): RF => {
+        const manifolds = [
+            ['normal', 'Normal'],
+            ['cylinder', 'Cylinder'],
+            ['mobius', 'Mobius strip'],
+            ['torus', 'Torus'],
+            ['klein', 'Klein bottle'],
+            ['projective', 'Projective plane'],
+        ];
+        const sizes = ['9x9', '13x13', '19x19'];
+
+        const choiceHandler = (
+            manifold: string,
+            name: string,
+            size: string
+        ) => (e: Event) => {
+            if (confirm(`Start ${size} ${manifold} game?`)) {
+                dispatch({
+                    ty: 'startPlaying',
+                    manifold: { ty: manifold, name },
+                });
+            }
+        };
+
+        const choicesRow = (manifold: string, name: string): RF => {
+            return row(
+                $b(T(name)),
+                ...sizes.map((size) =>
+                    button(choiceHandler(manifold, name, size))(T(size))
+                )
+            );
+        };
+
+        return $_(
+            T('Start a game at this table:'),
+            tabularize(...manifolds.map(([m, n]) => choicesRow(m, n)))
         );
+    },
 
-    return $_(
-        $_(T('Start a game at this table:')),
-        $tabularize(
-            $row('normal', 'Normal'),
-            $row('cylinder', 'Cylinder'),
-            $row('mobius', 'Mobius strip'),
-            $row('torus', 'Torus'),
-            $row('klein', 'Klein bottle'),
-            $row('projective', 'Projective plane')
-        )
-    );
+    playing: (state: State, dispatch: Dispatch): RF =>
+        $_(
+            $b(T('Game in progress...')),
+            button((e) => dispatch({ ty: 'backToStart' }))(T('Back to start'))
+        ),
 };
 
+const render = (state: State, dispatch: Dispatch): RF =>
+    renderAt[state.at](state, dispatch);
+
 (window as any).startGame = (init: TableDetail, game: Element): void => {
+    const s = { state: initialState(), pending: false };
     function update() {
-        patchIncrementalDom(game, () => render());
+        s.pending = false;
+        function dispatch(action: Action) {
+            s.state = reduce(s.state, action);
+            if (!s.pending) {
+                s.pending = true;
+                setTimeout(update, 0);
+            }
+        }
+        patchIncrementalDom(game, render(s.state, dispatch));
     }
     update();
 };
